1c4a40cc9d6e841c134731f0f789196177d61bac
PATRICK HESSION <G00394619@gmit.ie>
2024-01-09 17:04:54 +0000

    added layout temp

---
+++ src/layout.js
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+
+import React from 'react'
+
+const layout = () => {
+  return (
+    <main>
+      
+    </main>
+  )
+}
+
+export default layout
+
+
+
